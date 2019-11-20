6d8e928d4a6b660f9b256eeb9a3f925e074f4172
Baruch Phillips <baruchphillips@gmail.com>
2019-11-20 20:53:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
+require_relative 'dog.rb'
+require_relative 'cat.rb'
+require 'pp'
 class Owner
-  # code goes here
+ attr_reader :name, :species
+ 
+ @@all = []
+ def initialize(name,species='human') 
+   @name = name 
+   @species = species 
+   @@all << self 
+  end
+  def cats 
+  
+end 
+
+ def self.count 
+  @@all.size
+end 
+
+  def say_species
+     'I am a human.'
+end 
+
+ def self.all 
+   @@all 
+ end 
+ def self.reset_all 
+   @@all = []
+ end 
 end
