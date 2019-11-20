a446df1026a0240b1caac2b47ac26fc959add3c8
Baruch Phillips <baruchphillips@gmail.com>
2019-11-20 21:06:19 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -14,7 +14,7 @@ class Owner
     Cat.all.select{|cat| cat.owner == self}
   end
   def dogs 
-    Dog.all.select{|dog| dog.owner == self|
+    Dog.all.select{|dog| dog.owner == self}
   end 
  def self.count 
   @@all.size
